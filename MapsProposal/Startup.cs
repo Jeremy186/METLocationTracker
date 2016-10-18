33d9b0d6fad2f7d1d6e925a521dfd3f2f8e61732
womplord <jeremy186@hotmail.com>
2016-10-18 22:01:30 +0100

    did almost nothing today

--- MapsProposal/Startup.cs
+++ MapsProposal/Startup.cs
@@ -2,6 +2,9 @@
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
+using MapsProposal.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
 
 [assembly: OwinStartup(typeof(MapsProposal.Startup))]
 
@@ -12,6 +15,44 @@ namespace MapsProposal
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesAndUsers();
+        }
+
+        private void createRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            var a = roleManager.FindByName("Admin");
+            roleManager.Delete(a);
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.Email = "jeremybennett186@gmail.com";
+
+                string userPWD = "Stoneydeep1!";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
         }
     }
 }
